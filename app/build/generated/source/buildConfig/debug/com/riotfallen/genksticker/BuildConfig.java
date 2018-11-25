de05c60977e59536f9b2061401a3e2d2fc1fad19
dimassaputra <dimas.maulanads@gmail.com>
2018-11-25 15:07:55 +0800

    Versioning Start From HERE !

--- app/build/generated/source/buildConfig/debug/com/riotfallen/genksticker/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/riotfallen/genksticker/BuildConfig.java
@@ -9,7 +9,7 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final String VERSION_NAME = "0.5.251118";
   // Fields from default config.
   public static final String CONTENT_PROVIDER_AUTHORITY = "com.riotfallen.genksticker.stickercontentprovider";
 }
